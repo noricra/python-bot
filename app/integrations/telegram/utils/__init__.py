5d8e3a35c7b895354ce0f1f2ca9917c8f80d9d6d
noricra <noricra.tetra@gmail.com>
2026-01-21 00:14:43 -0500

    Add create_product_success_message to exports

--- app/integrations/telegram/utils/__init__.py
+++ app/integrations/telegram/utils/__init__.py
@@ -1,5 +1,5 @@
 """Telegram utilities"""
 
-from .message_utils import safe_transition_to_text
+from .message_utils import safe_transition_to_text, create_product_success_message
 
-__all__ = ['safe_transition_to_text']
+__all__ = ['safe_transition_to_text', 'create_product_success_message']
